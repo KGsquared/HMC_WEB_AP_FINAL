d41614309f127bcff6f878ea7e9f6c476d08ac3e
KGsquared <61001037+KGsquared@users.noreply.github.com>
2023-04-28 13:29:22 +0200

    added post method

--- HMC_WEB_APPLICATION/src/java/hmc_controller/LoginServlet.java
+++ HMC_WEB_APPLICATION/src/java/hmc_controller/LoginServlet.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +22,7 @@ import javax.servlet.http.HttpSession;
  *
  * @author k_gsquared
  */
+@WebServlet("/LoginServlet.do")
 public class LoginServlet extends HttpServlet {
     // statement to execute when user logs in 
     private Connection conn = null;
